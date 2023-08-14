4a1405436110225c04ce6e4ee7f857f7cd4d02f1
U-leee <samjok0923@naver.com>
2023-08-14 10:37:01 +0900

    add terraform cloud

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 terraform {
-  // cloud {
-  //   organization = "<MY_ORG_NAME>"         # 생성한 ORG 이름 지정
-  //   hostname     = "app.terraform.io"      # default
+  cloud {
+     organization = "u-lee-org"         # 생성한 ORG 이름 지정
+     hostname     = "app.terraform.io"      # default
 
-  //   workspaces {
-  //     name = "collaboration"  # 없으면 생성됨
-  //   }
-  // }
+     workspaces {
+       name = "terraform-aws-collaboration"  # 없으면 생성됨
+     }
+   }
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -17,6 +18,8 @@ terraform {
 
 provider "aws" {
   region = var.region
+  access_key = "AKIAVA6XPLMTIXVHJCL7"
+  secret_key = "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"
   default_tags {
     tags = {
       Project = "Coffee-Mug-Cake"
